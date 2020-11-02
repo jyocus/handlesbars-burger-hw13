b156151a39b06369f36259a7e0cbb701da537a94
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-01 19:46:46 -0600

    schema and seeds added as well as required express

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Patty Melt');
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (name, devoured) VALUES ('Slugburger', true);
