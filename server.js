4f7658a8bbf7ca32efb35fc3c579594a4f248b68
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-14 12:37:19 -0600

    fixed server connection, previous PORT was not defined

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var exphbs = require("express-handlebars");
 
 var app = express();
 
+var PORT = process.env.PORT || 3000;
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -21,8 +28,4 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+
