1dd4463551d7ebb7153fbf7f41fc4c9fccd83eaf
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-14 14:34:52 -0600

    fixed routes in controller and adjusted index handlesbars to the new schema

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,32 +17,22 @@ router.get("/", function(req, res) {
     });
   });
   //route for new burgers the user has created
-  router.post("/api/cats", function(req, res) {
+  router.post("/api/burgers", function(req, res) {
     burger.create([
-      "burger_name", "devoured"
+      "name", "devoured"
     ], [
       req.body.name, false
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+    ], function(data) {
+      res.send("Worked!!!");
     });
   });
   
   //Route will grab data for "devoured true/false"
   router.put("/api/devoured/:burgerId", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
+  //console.log("devoured route")
     burger.update({
-      devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+      devoured: true
+    }, "id =" + req.params.putID, function (data){
     });
   });
   
